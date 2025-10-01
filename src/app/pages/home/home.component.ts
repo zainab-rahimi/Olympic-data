de252155c9d0c0334e7f74926cf78a5ccf7e3072
Zainab Rahimi <zainab.rahimi@amadeus.com>
2025-10-01 09:49:52 +0200

    update the angular to @angular/cli@19

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,9 +3,10 @@ import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: false
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
